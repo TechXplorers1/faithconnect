67092db7f7a4300ae9a043f1c9254d2e04f0aeed
Resham Humer <reshamhumer.tx@gmail.com>
2025-11-19 05:05:51 +0000

    i said occupy the same width as dashboard for all the other pages , serm

--- src/app/admin/donations/page.tsx
+++ src/app/admin/donations/page.tsx
@@ -1,4 +1,5 @@
 
+
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { DONATIONS } from '@/lib/placeholder-data';
